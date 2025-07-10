649e84e0eae91e1ff8f4b9c46df34a366a37c9ff
Mario Ferrero <47794126+mariofdezzz@users.noreply.github.com>
2025-07-10 19:32:27 +0100

    feat: probar vercel config

---
+++ api/index.js
@@ -0,0 +1,27 @@
+import compression from "compression";
+import express from "express";
+import serveStatic from "serve-static";
+import serverless from "serverless-http";
+
+const app = express();
+
+app.use(compression());
+app.use(
+  "/",
+  serveStatic.default(resolve("dist/static"), {
+    index: false,
+    extensions: ["html"],
+    setHeaders(res) {
+      res.setHeader("Content-Type", "image/svg+xml");
+      res.setHeader(
+        "Cache-Control",
+        "no-store, no-cache, must-revalidate, proxy-revalidate"
+      );
+      res.setHeader("Pragma", "no-cache");
+      res.setHeader("Expires", "0");
+    },
+  })
+);
+
+export default app;
+export const handler = serverless(app);
