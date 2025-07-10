cb200a7e768f14cc2c21d5166905d5850948fa1c
Mario Ferrero <47794126+mariofdezzz@users.noreply.github.com>
2025-07-10 19:44:06 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,12 @@
 import compression from "compression";
 import express from "express";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import serveStatic from "serve-static";
 import serverless from "serverless-http";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const resolve = (p) => path.resolve(__dirname, p);
 
 const app = express();
 
